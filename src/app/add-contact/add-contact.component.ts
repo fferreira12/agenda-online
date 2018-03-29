3f0d738baa4e1559ec78037bf5a7175fc6de981e
Fernando Rodrigues Ferreira <fferreira12@gmail.com>
2018-03-29 17:01:48 -0300

    Changes Add Contact Component to use Reactive Forms.

--- src/app/add-contact/add-contact.component.ts
+++ src/app/add-contact/add-contact.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Contact } from '../contact';
-import { NgForm } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-contact',
@@ -10,47 +10,92 @@ import { NgForm } from '@angular/forms';
 export class AddContactComponent implements OnInit {
 
   private onAddMode: boolean = false;
-  private contact: Contact;
-  @ViewChild('f') contactForm: NgForm;
+  private contactForm: FormGroup;
 
-  private dataItems = {
-    telefone: 1,
-    email: 1,
-    endereco: 1,
-    redesSociais: 1
+  constructor(private fb: FormBuilder) {
+    this.createForm();
   }
 
-  constructor() { }
-
   ngOnInit() {
-    //this.contact = new Contact();
+    this.addItem('telefone');
+    this.addItem('email');
+    this.addItem('url');
+    this.addItem('endereco');
+    this.addItem('rede_social');
+  }
+
+  createForm() {
+    this.contactForm = this.fb.group({
+      nome: ['', Validators.required],
+      nome_fonetico: '',
+      sobrenome: '',
+      sobrenome_fonetico: '',
+      empresa: '',
+      empresa_fonetico: '',
+      telefones: this.fb.array([]),
+      emails: this.fb.array([]),
+      urls: this.fb.array([]),
+      enderecos: this.fb.array([]),
+      redes_sociais: this.fb.array([])
+    });
+  }
+
+  setTelefones(telefones: { tipo: String, numero: String }[]) {
+    const telefoneFGs = telefones.map(telefone => this.fb.group(telefone));
+    const telefoneFormArray = this.fb.array(telefoneFGs);
+    this.contactForm.setControl('telefones', telefoneFormArray);
+  }
+
+  addTelefone() {
+    this.telefones.push(this.fb.group({ tipo: '', numero: '' }));
+  }
+
+  addItem(type: String) {
+    if (type === 'telefone') {
+      this.getItem('telefone').push(this.fb.group({ tipo: '', numero: '' }));
+    } else if (type === 'email') {
+      this.getItem('email').push(this.fb.group({ tipo: '', email: '' }));
+    } else if (type === 'url') {
+      this.getItem('url').push(this.fb.group({ tipo: '', url: '' }));
+    } else if (type === 'endereco') {
+      this.getItem('endereco').push(this.fb.group({ tipo: '', endereco: '' }));
+    } else if (type === 'rede_social') {
+      this.getItem('rede_social').push(this.fb.group({ rede: '', url: '' }));
+    }
+  }
+
+  removeTelefone(index: number) {
+    this.telefones.removeAt(index);
+  }
+
+  removeItem(type: String, index: number) {
+    this.getItem(type).removeAt(index);
+  }
+
+  get telefones(): FormArray {
+    return this.contactForm.get('telefones') as FormArray;
+  };
+
+  getItem(type: String): FormArray {
+    if (type === 'telefone') {
+      return this.contactForm.get('telefones') as FormArray;
+    } else if (type === 'email') {
+      return this.contactForm.get('emails') as FormArray;
+    } else if (type === 'url') {
+      return this.contactForm.get('urls') as FormArray;
+    } else if (type === 'endereco') {
+      return this.contactForm.get('enderecos') as FormArray;
+    } else if (type === 'rede_social') {
+      return this.contactForm.get('redes_sociais') as FormArray;
+    }
   }
 
   onClickAddContact() {
     this.onAddMode = !this.onAddMode;
   }
 
-  onAddItem(type: string) {
-    this.dataItems[type] += 1;
-  }
-
-  onRemoveItem(type: string, item: any) {
-    let index = this.contact[type].indexOf(item);
-    if (index > -1) {
-      this.dataItems[type].splice(index, 1);
-    }
-  }
 
   onSave() {
-    this.contact = new Contact({
-      nome: this.contactForm.value['nome'],
-      nome_fonetico: this.contactForm.value['nome_fonetico'],
-      sobrenome: this.contactForm.value['sobrenome'],
-      sobrenome_fonetico: this.contactForm.value['sobrenome_fonetico'],
-      empresa: this.contactForm.value['empresa'],
-      empresa_fonetico: this.contactForm.value['empresa_fonetico'],
-      
-    });
-    console.log(this.contact);
+
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
@@ -21,7 +21,8 @@ import { AddContactComponent } from './add-contact/add-contact.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     ContactMockup,
